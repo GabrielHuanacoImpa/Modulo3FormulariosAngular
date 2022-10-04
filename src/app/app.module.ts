40a835ff3bbefda730740ff53774d12ff35976d8
GabrielHuanacoImpa <gabrielhuanaco@gmail.com>
2022-10-04 19:49:30 -0300

    probandoPat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent, LoginComponent ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+// importamos las librerias de formulario que vamos a necesitar
+import { FormBuilder, FormGroup, Validators,FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+
+  // Inyectar en el constructor el formBuilder
+  constructor(private formBuilder: FormBuilder){ 
+    ///Creamos el grupo de controles para el formulario de login
+    this.form= this.formBuilder.group({
+      usuario:['',[Validators.required, Validators.minLength(5)]],
+
+      password:['',[Validators.required, Validators.minLength(8)]],
+      email:['', [Validators.required, Validators.email]],
+   })
+  }
+
+  ngOnInit() {}
+
+  get Usuario(){
+    return this.form.get("usuario");
+  }
+  get Password(){
+    return this.form.get("password");
+  }
+ 
+  get Mail(){
+   return this.form.get("email");
+  }
+
+  get UsuarioValid(){
+    return this.Usuario?.touched && !this.Usuario?.valid;
+  }
+  get PasswordValid(){
+    return this.Password?.touched && !this.Password?.valid;
+  }
+
+  get MailValid() {
+    return false
+  }
+ 
+
+  onEnviar(event: Event){
+    // Detenemos la propagación o ejecución del compotamiento submit de un form
+    event.preventDefault; 
+ 
+    if (this.form.valid){
+      // Llamamos a nuestro servicio para enviar los datos al servidor
+      // También podríamos ejecutar alguna lógica extra
+      alert("Todo salio bien ¡Enviar formuario!")
+    }else{
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+      this.form.markAllAsTouched(); 
+    }
+ 
+  }
+
+
+}
